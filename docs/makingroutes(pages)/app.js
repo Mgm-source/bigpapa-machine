4e6ead63f333cb3fdd5cf7ede03ef54cb6777617
Mgm-source <55754333+Mgm-source@users.noreply.github.com>
2020-01-20 23:56:46 +0000

    Rework of folders

---
+++ docs/makingroutes(pages)/app.js
@@ -0,0 +1,34 @@
+angular.module('routingApp',['ui.router'])
+.controller("routingAppController",function($state){
+    // needed otherwise js doesnt know where to look
+    var self = this;
+// html go button; reference basically 
+    self.go = function(page){
+        //$go Transition to a different state
+        $state.go(page);
+        console.log($state.go(page));
+    };
+
+})
+ // basically a constructor  just choose the same name as the services just not to get confused 
+.config(['$stateProvider','$urlServiceProvider',function($stateProvider,$urlServiceProvider){
+
+    $stateProvider
+    // different states 
+    .state('Index', {
+        Url:'/Index',
+        templateUrl:"views/index.html",
+
+       
+    })
+    // different states 
+    .state('Name', {
+        Url:'/Name',
+        templateUrl:"views/Entername.html",
+ 
+    })
+    // this has the otherwise stuff
+    $urlServiceProvider.rules
+    .otherwise('/Index');
+    
+}]);
