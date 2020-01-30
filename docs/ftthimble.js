d1dc281ffe60e3f18650696fa9bf6ecb0c345ffb
Mgm-source <55754333+Mgm-source@users.noreply.github.com>
2020-01-30 13:55:33 +0000

    Update ftthimble.js

--- docs/ftthimble.js
+++ docs/ftthimble.js
@@ -1,4 +1,16 @@
-        var x, y, randomthimbleX, randomthimbleY = 0;
+function createArray(length) {
+    var arr = new Array(length || 0),
+        i = length;
+
+    if (arguments.length > 1) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        while(i--) arr[length-1 - i] = createArray.apply(this, args);
+    }
+
+    return arr;
+}
+
+var x, y, randomthimbleX, randomthimbleY = 0;
 
         var Guess = 1;
 
@@ -6,7 +18,7 @@
 
         const thimbleCheat = "#t";
 
-        const gameroom = new Array(2);
+        const gameroom = new createArray(10,10);
 
         var Yesno;
 
