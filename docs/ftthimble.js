f82bb6249ee74055b240a2171b2e8e84c6003038
Mgm-source <55754333+Mgm-source@users.noreply.github.com>
2020-01-30 13:36:31 +0000

    Update ftthimble.js
    Added the variables.

--- docs/ftthimble.js
+++ docs/ftthimble.js
@@ -1,1 +1,12 @@
-alert("hello");
+        var x, y, randomthimbleX, randomthimbleY = 0;
+
+        var Guess = 1;
+
+        const thimbleplaceholder = " "; //"thimble"
+
+        const thimbleCheat = "#t";
+
+        const gameroom = new Array(2);
+
+        const Yesno;
+
