8e00c86c94ca96c9757b0b090b868a407cb7e57b
Mgm-source <55754333+Mgm-source@users.noreply.github.com>
2020-01-31 05:06:04 +0000

    Update ftthimble.js

--- docs/ftthimble.js
+++ docs/ftthimble.js
@@ -1,1 +1,19 @@
-alert("hello");
+//Makes sure that the web page is fully loaded before it starts working/listening
+document.addEventListener("DOMContentLoaded",startupWebPage);
+
+function startupWebPage()
+{ 
+ // declaring the canvas 
+ const cvs = document.getElementById("canvas");
+ const ctx = cvs.getContext("2d");
+
+ cvs = document.getElementById("scale");
+
+ var x,y,guess,randtx,randty,col,row;
+ var thimble,thimble2,Yesno;
+ var  gameroom = new array[10,10];
+ function draw()
+ {
+   
+ }
+}
