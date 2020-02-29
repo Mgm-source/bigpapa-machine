eec0b4c59ed0b2e0671d74d35d462e7a48694725
Mgm-source <55754333+Mgm-source@users.noreply.github.com>
2020-02-29 09:27:39 +0000

    changes

---
+++ docs/MqttJs/mqtt.js
@@ -0,0 +1,32 @@
+// making the connect variables
+var mqtt;
+var reconnectTimeout=2000;
+var host="127.0.0.1";
+var port=1883;
+
+function onConnect(){
+    // If there's a connection subscribe to the mqtt and send a message
+    console.log("connected");
+    //mqtt.subscribe("senor1");
+    message = new Paho.MQTT.Message("hello World");
+
+     // topic sensor1
+   message.destinationName = "sensor1";
+  // publish topic sensor1
+   mqtt.send(message);
+
+}
+
+function MQTTconnect(){
+    // show connection details to the console
+    console.log("connecting to "+host+":"+port);
+    //Creating a client object 
+    mqtt = new Paho.MQTT.Client(host,port,"clientjs");
+    var options = {
+        timeout: 3,
+        onSuccess: onConnect,
+    };
+    // connects to the server
+    mqtt.connect(options);
+}
+
