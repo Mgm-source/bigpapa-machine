06eb84fd1ca158ac46f019f87f2ba6d9f7ca5467
Mgm-source <55754333+Mgm-source@users.noreply.github.com>
2022-02-12 20:04:52 +0000

    working with helpful additions

--- docs/strafeperfector/script.js
+++ docs/strafeperfector/script.js
@@ -1,10 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
+    let averageTime = null;
 
     let currentStack = [];
     let startKey = null;
     let counter = 0;
+    let errorCounter = 0;
+    let timedHits = [];
 
     const dStack = ["d", "a", "click"];
     const aStack = ["a", "d", "click"];
@@ -13,7 +16,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function captureEvent(event, valid) {
         let correctKey = false;
-
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.font = '10px Arial';
         if (valid) {
 
             if (counter === 0) {
@@ -23,30 +27,53 @@ document.addEventListener("DOMContentLoaded", function () {
             if (startKey) {
 
                 switch (event.key) {
-                    case "w": currentStack.push(event.key);
+                    case "w": currentStack.push({ "key": event.key, "timeStamp": event.timeStamp });
                         break;
-                    case "a": currentStack.push(event.key);
+                    case "a": currentStack.push({ "key": event.key, "timeStamp": event.timeStamp });
                         break;
-                    case "s": currentStack.push(event.key);
+                    case "s": currentStack.push({ "key": event.key, "timeStamp": event.timeStamp });
                         break;
-                    case "d": currentStack.push(event.key);
+                    case "d": currentStack.push({ "key": event.key, "timeStamp": event.timeStamp });
                         break;
-                    default: if (event.type === "click") currentStack.push(event.type);
+                    default: if (event.type === "click") currentStack.push({ "key": event.type, "timeStamp": event.timeStamp });
                         break;
                 }
 
                 switch (startKey) {
-                    case "w": correctKey = currentStack[counter] == wStack[counter];
+                    case "w": correctKey = currentStack[counter].key == wStack[counter];
                         break;
-                    case "a": correctKey = currentStack[counter] == aStack[counter];
+                    case "a": correctKey = currentStack[counter].key == aStack[counter];
                         break;
-                    case "s": correctKey = currentStack[counter] == sStack[counter];
+                    case "s": correctKey = currentStack[counter].key == sStack[counter];
                         break;
-                    case "d": correctKey = currentStack[counter] == dStack[counter];
+                    case "d": correctKey = currentStack[counter].key == dStack[counter];
                         break;
+                    default: counter = 3;
                 }
 
+
             }
+
+            if (currentStack.length > 1) {
+                ctx.fillText("Time in Milliseconds : " + (currentStack[counter].timeStamp - currentStack[counter - 1].timeStamp).toFixed(2), 0, 50);
+            }
+
+            if (currentStack.length == 3) {
+
+                ctx.fillText("Time in Milliseconds : " + ((currentStack[1].timeStamp - currentStack[0].timeStamp) + (currentStack[2].timeStamp - currentStack[1].timeStamp)).toFixed(2), 0, 60);
+
+                if (correctKey) {
+                    timedHits.push(((currentStack[1].timeStamp - currentStack[0].timeStamp) + (currentStack[2].timeStamp - currentStack[1].timeStamp)).toFixed(2));
+
+                    averageTime = timedHits.reduce(function (prev, curr) {
+                        return (Number(prev) + Number(curr));
+                    }) / timedHits.length;
+                }
+
+
+            }
+
+            if (!correctKey) errorCounter++;
 
             counter++;
 
@@ -55,11 +82,12 @@ document.addEventListener("DOMContentLoaded", function () {
                 startKey = null;
                 currentStack = [];
 
+
             }
 
         }
-
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        if (timedHits.length != 0) ctx.fillText("Average Time in Milliseconds : " + averageTime.toFixed(2), 0, 70);
+        ctx.fillText("Number of errors: " + errorCounter, 0, 80);
         ctx.font = '50px Arial';
         correctKey ? ctx.fillStyle = "green" : ctx.fillStyle = "red";
         ctx.fillText((event.type === "click") ? "click" : event.key, canvas.width / 2, canvas.height / 2);
