7576fe0f22f235ef1d6f4ada220c9cc23282020b
Mgm-source <55754333+Mgm-source@users.noreply.github.com>
2022-02-13 12:21:55 +0000

    fixed centering for start prompt

--- docs/strafeperfector/script.js
+++ docs/strafeperfector/script.js
@@ -5,9 +5,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
-    ctx.font = '50px Arial';
+    ctx.font = '24px Arial';
     ctx.fillStyle = "orange";
-    ctx.fillText("Input WASD to START", canvas.width / 2, canvas.height / 2);
+    ctx.fillText("Strafe to START", canvas.width / 2 - 72, canvas.height / 2);
 
     const dStack = ["d", "a", "mouseClick"];
     const aStack = ["a", "d", "mouseClick"];
