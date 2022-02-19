e3dd45829a0c9c6f12fcb31498551faf3bdb0a6a
Munashe <munashe.m@outlook.com>
2022-02-19 13:04:48 +0000

    update

--- cplus/beginning/Source.cpp
+++ cplus/beginning/Source.cpp
@@ -2,14 +2,18 @@
 #include <iostream>
 using namespace std;
 
+template<typename T>
 class Vector {
 public:
 	Vector(int size) {
 		if (size < 0) throw length_error{ "Vector Constructor size" };
-		element = new double[size];
+		element = new T[size];
 		length = size;
 	}
-	double& operator[](int i) {
+
+	~Vector() { delete[] element; };
+
+	T& operator[](int i) {
 		if (i < 0 || size() <= i) throw out_of_range{ "Vector::operator[]" };
 		return element[i]; 
 	}
@@ -17,20 +21,28 @@ public:
 		return length;
 	}
 private:
-	double* element;
+	T* element;
 	int length;
 };
 
-Vector v(10);
+template<typename T>
+T* begin(Vector<T>& element) {
+	return element.size() ? &element[0] : nullptr;
+};
+template<typename T>
+T* end(Vector<T>& element) {
+	return begin(element) + element.size();
+};
 
+Vector<int> v(10);
 
 int main() {
 	for (int i = 0; i < v.size(); i++) {
-		v[i] = i+0.5;
+		v[i] = i;
 	}
 
-	for (int i = 0; i < v.size(); i++) {
-		cout << v[i] << "\n";
+	for (auto element : v) {
+		cout << element << "\n";
 	}
 
 	return 0;
