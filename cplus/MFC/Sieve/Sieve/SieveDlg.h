1d5cf515405522f9e73fc828ca7cb9b4838c75ed
Munashe Matadi <Munashe.m@outlook.com>
2022-11-01 03:31:17 +0000

    sieve multithreaded

---
+++ cplus/MFC/Sieve/Sieve/SieveDlg.h
@@ -0,0 +1,37 @@
+
+// SieveDlg.h : header file
+//
+
+#pragma once
+
+// CSieveDlg dialog
+class CSieveDlg : public CDialogEx
+{
+// Construction
+public:
+	CSieveDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_SIEVE_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnStart();
+protected:
+	afx_msg LRESULT OnUserThreadFinished(WPARAM wParam, LPARAM lParam);
+};
